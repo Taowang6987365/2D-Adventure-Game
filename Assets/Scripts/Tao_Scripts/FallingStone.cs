99db69070e81dc27bf86ce93ad1d9c2ee1239f64
LAPTOP-BQ6I1IAM\TaoWang <taowang.qc@gmail.com>
2021-06-27 18:56:23 -0400

    Adjust fallingstone script

--- Assets/Scripts/Tao_Scripts/FallingStone.cs
+++ Assets/Scripts/Tao_Scripts/FallingStone.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FallingStone : MonoBehaviour
 {
     public Vector3 stoneVelocity;
     public Controller2D controller;
+    public float fallingSpeed;
     public float dirction;
 
     void Start()
     {
         controller = GetComponent<Controller2D>();
+        fallingSpeed = 0.1f;
     }
 
     private void FixedUpdate()
@@ -36,11 +39,11 @@ public class FallingStone : MonoBehaviour
 
         if (dirction < 0)
         {
-            velocity.y += PlayerController.gravity * Time.fixedDeltaTime * 0.6f;
+            velocity.y += PlayerController.gravity * Time.fixedDeltaTime * fallingSpeed;
         }
         else
         {
-            velocity.y -= PlayerController.gravity * Time.fixedDeltaTime * 0.6f;
+            velocity.y -= PlayerController.gravity * Time.fixedDeltaTime * fallingSpeed;
         }
 
         controller.Move(velocity * Time.fixedDeltaTime, false);
