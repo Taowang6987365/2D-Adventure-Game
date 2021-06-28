68b73d208c804972ff6b8c9098569fb5cc801cdf
Hongfei-Liu <60199524+Hongfei-Liu@users.noreply.github.com>
2021-06-28 13:57:55 -0400

    add special bullet

---
+++ Assets/Scripts/Liu_Scripts/SpecialBullets/BulletCharacter.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCharacter : MonoBehaviour
+{
+    public Vector3 dir;
+    public float speed;
+    public bool isMove;
+    bool isBallModePlay;
+    [Range(6f, 10f)] [SerializeField] private float bulletSpeed = 7f;
+    void Start()
+    {
+        isMove = true;
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMove)
+        {
+            BulletMove();
+        }
+        transform.Translate(Vector3.down * bulletSpeed * Time.deltaTime);
+
+    }
+
+    public void BulletMove()
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    public IEnumerator DirChangeMoveMode(float endTime, float dirChangeTime, float angle)
+    {
+        float time = 0;
+        bool isRotate = true;
+        isBallModePlay = true;
+        while (isBallModePlay)
+        {
+            time += Time.deltaTime;
+            transform.position += speed * dir * Time.deltaTime;
+            if (time >= dirChangeTime && isRotate)
+            {
+                isRotate = false;
+                StartCoroutine(BulletRotate(angle));
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator BulletRotate(float angle)
+    {
+        while (isBallModePlay)
+        {
+            Quaternion tempQuat = Quaternion.AngleAxis(angle, Vector3.forward);
+            dir = tempQuat * dir;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
---
+++ Assets/Scripts/Liu_Scripts/SpecialBullets/ShotGunBullet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShotGunBullet : MonoBehaviour
+{
+    public BulletCharacter bulletTemplate;
+    public Transform firPoint;
+    private List<BulletCharacter> tempBullets;
+
+    public float CountTime;
+    public float StopTime;
+
+
+
+    void Start()
+    {
+        tempBullets = new List<BulletCharacter>();
+        CountTime *= Time.deltaTime;
+        StopTime += Time.deltaTime;
+        StartCoroutine(FirShotgun());
+    }
+
+    IEnumerator FirShotgun()
+    {
+        Vector3 bulletDir = Vector3.down;
+        Quaternion leftRota = Quaternion.AngleAxis(-30, Vector3.forward);
+        Quaternion RightRota = Quaternion.AngleAxis(30, Vector3.forward); 
+        while (CountTime < StopTime)
+        {
+            yield return new WaitForSeconds(2f);
+            for (int i = 0; i < 10; i++)     
+            {
+                for (int j = 0; j < 3; j++) 
+                {
+                    switch (j)
+                    {
+                        case 0:
+                            CreatBullet(bulletDir, firPoint.transform.position);  
+                            break;
+                        case 1:
+                            bulletDir = RightRota * bulletDir;
+                            CreatBullet(bulletDir, firPoint.transform.position);
+                            break;
+                        case 2:
+                            bulletDir = leftRota * (leftRota * bulletDir); 
+                            CreatBullet(bulletDir, firPoint.transform.position);
+                            bulletDir = RightRota * bulletDir; 
+                            break;
+                    }
+                }
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+
+    }
+
+    public BulletCharacter CreatBullet(Vector3 dir, Vector3 creatPoint)
+    {
+        BulletCharacter bulletCharacter = Instantiate(bulletTemplate, creatPoint, Quaternion.identity);
+        bulletCharacter.gameObject.SetActive(true);
+        bulletCharacter.dir = dir;
+        tempBullets.Add(bulletCharacter);
+        return bulletCharacter;
+    }
+}
