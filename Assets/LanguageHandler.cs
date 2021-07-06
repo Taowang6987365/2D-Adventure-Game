050f55f81a244245de3a014cce45b55bdab8c71d
wmh19931230 <wmhziyu@live.cn>
2021-07-06 14:17:40 -0400

    localization test main menu

---
+++ Assets/LanguageHandler.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LanguageHandler : MonoBehaviour
+{
+
+    public class Dialogs
+    {
+        public string[] eng;
+        public string[] chn;
+    }
+
+
+    private string path;
+    public Dialogs logs;
+    private string jsonstr;
+    public int currentLang =1;
+    public static LanguageHandler instance { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = "D:/Unity/Adventure/Assets/Language/Language.json";
+        jsonstr = System.IO.File.ReadAllText(path);
+        logs = JsonUtility.FromJson<Dialogs>(jsonstr);
+        currentLang = 1;
+        //Debug.Log(logs.eng[1]);
+    }
+    private void Update()
+    {
+        
+    }
+    public void ChangeLang(int lang)
+    {
+        currentLang = lang;
+    }
+    public string GetText(int logNum)
+    {
+        if(currentLang == 1)
+        {
+            return logs.eng[logNum];
+        }
+        else if(currentLang==2)
+        {
+            return logs.chn[logNum];
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+
+}
