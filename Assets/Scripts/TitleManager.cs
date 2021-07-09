026881f0dbf6b0e47692e5922b125ee2c49c5f19
LAPTOP-BQ6I1IAM\TaoWang <taowang.qc@gmail.com>
2021-07-08 21:29:16 -0400

    Nothing

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -15,6 +15,8 @@ public class TitleManager : MonoBehaviour
     [SerializeField] private Canvas levelCanvas;
     public GameManager gm;
 
+    //...
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
